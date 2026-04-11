538c6ef7485d89b3d2c9654b968def7f2c17da36
André Gabriel <andreon098@gmail.com>
2026-04-11 13:37:44 -0300

    fix: fix config module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { AuthService } from './auth.service'
 
 @Module({
   imports: [
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
